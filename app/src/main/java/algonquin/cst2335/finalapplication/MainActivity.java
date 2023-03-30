65de0b77693c92eb7c852006ac0e5caf3389d31d
Jad Jreige <jad.jreige@outlook.com>
2023-03-29 21:39:25 -0400

    Added the buttons for the main activity

--- app/src/main/java/algonquin/cst2335/finalapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalapplication/MainActivity.java
@@ -2,7 +2,15 @@ package algonquin.cst2335.finalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import algonquin.cst2335.finalapplication.KittenPlaceholder.KittenActivity;
+import algonquin.cst2335.finalapplication.NASA.MarsActivity;
+import algonquin.cst2335.finalapplication.NewYorkTimes.NewYorkActivity;
+import algonquin.cst2335.finalapplication.WeatherStack.WeatherActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nasa = findViewById(R.id.NASA);
+        Button kitten = findViewById(R.id.kitten);
+        Button newYork = findViewById(R.id.newYork);
+        Button weather = findViewById(R.id.weather);
+
+        nasa.setOnClickListener( clk -> {
+
+            Intent nasaPage = new Intent(MainActivity.this, MarsActivity.class);
+
+            startActivity(nasaPage);
+        });
+
+        kitten.setOnClickListener( clk -> {
+
+            Intent kittenPage = new Intent(MainActivity.this, KittenActivity.class);
+
+            startActivity(kittenPage);
+        });
+
+        newYork.setOnClickListener( clk -> {
+
+            Intent newYorkPage = new Intent(MainActivity.this, NewYorkActivity.class);
+
+            startActivity(newYorkPage);
+        });
+
+        weather.setOnClickListener( clk -> {
+
+            Intent weatherPage = new Intent(MainActivity.this, WeatherActivity.class);
+
+            startActivity(weatherPage);
+        });
     }
 }
