dc68fd678249af3a6f6409b132644f5b9d326702
Jad Jreige <jad.jreige@outlook.com>
2023-04-05 10:35:08 -0400

    first commit on nasa

--- app/src/main/java/algonquin/cst2335/finalapplication/NASA/MarsActivity.java
+++ app/src/main/java/algonquin/cst2335/finalapplication/NASA/MarsActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import algonquin.cst2335.finalapplication.R;
+import algonquin.cst2335.finalapplication.databinding.ActivityMarsBinding;
 
 public class MarsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mars);
+
+        ActivityMarsBinding binding = ActivityMarsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
